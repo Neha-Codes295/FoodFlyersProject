d91e2d280080dcca88e422724c32ac78d96aa5fa
Neha <neha.contact295@gmail.com>
2025-03-26 19:17:01 +0530

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://neha06012005:neha6125@cluster0.pdktpqx.mongodb.net/tomato').then(()=>console.log("DB Connected"));
-}
+export const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connected"));
+};
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,39 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import dotenv from "dotenv";
 /*************************.js */
 
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
+dotenv.config({
+  path: ".env",
+});
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
-// mongoose.connect("mongodb+srv://neha06012005:neha6125@cluster0.pdktpqx.mongodb.net/tomato");
-
 
 // api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-
-app.get("/", (req,res) => {
-    res.send("API Working")
-})
-
-app.listen(port, ()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
-
-// mongodb+srv://neha06012005:neha6125@cluster0.pdktpqx.mongodb.net/?
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
